bd715238a98dd7b5e430aab9c93afda5aaaf789d
Anna Kulibabina <anna.kulibabina@gmail.com>
2022-06-18 15:22:55 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,30 @@ import Section from './Section';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
+import contactsList from '../contacts.json';
 
 function App() {
-  const [contacts, setContacts] = useLocalStorage('contactList', []);
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? contactsList
+  );
   const [filter, setFilter] = useState('');
 
-  function useLocalStorage(key, defaultValue) {
-    const [state, setState] = useState(
-      () => JSON.parse(window.localStorage.getItem(key)) ?? defaultValue
-    );
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+  // const [contacts, setContacts] = useLocalStorage('contactList', []);
+  // const [filter, setFilter] = useState('');
 
-    useEffect(() => {
-      window.localStorage.setItem(key, JSON.stringify(state));
-    }, [key, state]);
-    return [state, setState];
-  }
+  // function useLocalStorage(key, defaultValue) {
+  //   const [state, setState] = useState(
+  //     () => JSON.parse(window.localStorage.getItem(key)) ?? defaultValue
+  //   );
+
+  //   useEffect(() => {
+  //     window.localStorage.setItem(key, JSON.stringify(state));
+  //   }, [key, state]);
+  //   return [state, setState];
+  // }
 
   const formSubmit = ({ name, number }) => {
     const isContact = contacts.find(contact => contact.name === name);
